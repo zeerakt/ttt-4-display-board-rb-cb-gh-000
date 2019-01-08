2ee7e477e18a81d2e6ab1a4ff9527540a099e3f9
Zeerak Tahir <ztahir.bee18seecs@seecs.edu.pk>
2019-01-08 15:06:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board  = ['X','O',' ',' ','O','X','O',' ','X']
+boards  = ['X','O',' ',' ','O','X','O',' ','X']
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -9,4 +9,4 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
   puts "-----------"
 end
-display_board
+display_board(boards)
